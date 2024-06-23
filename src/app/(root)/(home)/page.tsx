7e24fc4f5c3f67f9452eb3ab57cd29c1a776f280
Austin Ameh <arewageek@gmail.com>
2024-06-23 15:35:00 +0100

    Title reverted

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -12,11 +12,11 @@ const HomePage = () => {
         <section className='flex flex-col size-full gap-10'>
             <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
                 <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-                    <h2 className='glassmorphism w-fit rounded py-2 px-5 text-center text-3xl font-bold'>
-                        {/* <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-medium'> */}
-                        {/* Upcoming meeting at 12:30 PM */}
+                    {/* <h2 className='glassmorphism w-fit rounded py-2 px-5 text-center text-3xl font-bold'> */}
+                    <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-medium'>
+                        Upcoming meeting at 12:30 PM
 
-                        Happy Birthday Barak!!!
+                        {/* Happy Birthday Barak!!! */}
                     </h2>
                     <div className='flex flex-col gap-2'>
                         <h1 className='text-4xl font-extrabold lg:text-7xl'>
