16e5a84d92482718e13bb65f4da3618af864ba7a
Austin Ameh <arewageek@gmail.com>
2024-06-24 00:16:44 +0100

    redeploy

--- src/app/(root)/(home)/upcoming/page.tsx
+++ src/app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,12 @@
+import CallList from '@/components/meeting/CallList'
 import React from 'react'
 
 const Upcoming = () => {
     return (
         <section className='flex flex-col size-full gap-10'>
             <h1 className='text-3xl font-bold'>Upcoming</h1>
+
+            <CallList type="upcoming" />
         </section>
     )
 }
