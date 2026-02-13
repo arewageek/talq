5903e0b22baa457e95aed84b8801d05bc31320c1
Arewa Geek <arewageek@gmail.com>
2026-02-13 18:44:52 +0100

    Refactor: Update Clerk protected routes and enhance middleware matcher configuration.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoute = createRouteMatcher([
-  "/upcomings",
-  "previous",
-  "recording",
+const isProtectedRoute = createRouteMatcher([
+  "/upcoming",
+  "/previous",
+  "/recordings",
   "/personal",
-  "/meetings(.*)",
-  "/meetings",
-  "/meetings/(.*)",
+  "/meeting(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) {
-    auth().protect();
-  }
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
